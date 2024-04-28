dad806b2e38b9f7dfc55ad082b02e67e01e7f8dc
iampraharsh <praharsh.tembhurne@gmail.com>
2024-04-28 19:17:43 +0530

    Checkout/ProductDetails Page Added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.css';
-
-import Home from './pages/Home';
-import LoginPage from './pages/LoginPage';
-import SignUpPage from './pages/SignUpPage';
-import CartPage from './pages/CartPage';
-
 import {
 	createBrowserRouter,
 	RouterProvider,
 	Route,
 	Link,
 } from 'react-router-dom';
+import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+import CartPage from './pages/CartPage';
+import Checkout from './pages/Checkout';
+import ProductDetailsPage from './pages/ProductDetailsPage';
 
 const router = createBrowserRouter([
 	{
@@ -29,6 +29,14 @@ const router = createBrowserRouter([
 	{
 		path: '/cart',
 		element: <CartPage></CartPage>,
+	},
+	{
+		path: '/checkout',
+		element: <Checkout></Checkout>,
+	},
+	{
+		path: '/product-details',
+		element: <ProductDetailsPage></ProductDetailsPage>,
 	},
 ]);
 
